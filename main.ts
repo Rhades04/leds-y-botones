4924740d86ddc6ec903d1ac2ee6c99dc84b3c682
Rhades04 <100341076+Rhades04@users.noreply.github.com>
2022-02-24 08:33:06 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Hades")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Heart)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Merced")
+})
+basic.forever(function () {
+	
+})
